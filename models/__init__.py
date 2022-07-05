0f30824308c4597fa0f14b9931268755cfcca9ba
HELLORPG <hellorpg2017@gmail.com>
2022-07-05 20:32:57 +0800

    Add ResNet18.

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# @Author       : Ruopeng Gao
+# @Date         : 2022/7/5
+# @Description  : __init__.py
+
+
---
+++ models/resnet18.py
@@ -0,0 +1,18 @@
+# @Author       : Ruopeng Gao
+# @Date         : 2022/7/5
+# @Description  : Create a ResNet18 Model.
+
+
+import torchvision
+
+import torch.nn as nn
+
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(ResNet18, self).__init__()
+
+        self.model = torchvision.models.resnet18(pretrained=False)
+
+    def forward(self, x):
+        return self.model(x)
