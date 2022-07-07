78fac968c39689da3a11ccd35e4bc5437f3d015f
HELLORPG <hellorpg2017@gmail.com>
2022-07-07 21:18:35 +0800

    Update some code about training.

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@
 # @Date         : 2022/7/5
 # @Description  : Train and Evaluation functions, mainly used in main.py.
 import torch
+import torch.nn as nn
 
 from models.build import build_model
 
@@ -16,5 +17,21 @@ def train(config: dict):
     model = build_model(config=config)
     model.to(device=torch.device(config["DEVICE"]))
 
+    for epoch in config["TRAIN"]["EPOCHS"]:
+        pass
+
     print("Here")
+
+
+def train_one_epoch(model: nn.Module, config: dict, epoch: int):
+    """
+    Args:
+        model: Model.
+        config: Main config.
+        epoch: Current epoch.
+
+    Returns:
+        Logs
+    """
+    pass
 
