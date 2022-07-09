a79b4357bc21dfc1ae3b247f1f4b30fbbfef37d4
HELLORPG <hellorpg2017@gmail.com>
2022-07-09 22:07:57 +0800

    Add evaluation code.

--- engine.py
+++ engine.py
@@ -23,22 +23,33 @@ def train(config: dict):
     model = build_model(config=config)
     model.to(device=torch.device(config["DEVICE"]))
 
-    dataloader = build_dataloader(
+    train_dataloader = build_dataloader(
         dataset=config["DATA"]["DATASET"],
         root=config["DATA"]["DATA_PATH"],
         split="train",
         bs=config["TRAIN"]["BATCH_SIZE"]
     )
 
+    test_dataloader = build_dataloader(
+        dataset=config["DATA"]["DATASET"],
+        root=config["DATA"]["DATA_PATH"],
+        split="test",
+        bs=config["TRAIN"]["BATCH_SIZE"]*2
+    )
+
     loss_function = nn.CrossEntropyLoss()
     optimizer = Adam(params=model.parameters(), lr=config["TRAIN"]["LR"])
 
     for epoch in range(0, config["TRAIN"]["EPOCHS"]):
-        train_one_epoch(model=model, dataloader=dataloader, loss_function=loss_function, optimizer=optimizer,
-                        config=config, epoch=epoch)
-        pass
+        train_log = train_one_epoch(model=model, dataloader=train_dataloader, loss_function=loss_function,
+                                    optimizer=optimizer,
+                                    config=config,
+                                    epoch=epoch)
+        test_log = evaluate(model=model, dataloader=test_dataloader, loss_function=loss_function,
+                            config=config)
+        print(train_log, test_log)
 
-    print("Here")
+    # print("Here")
 
 
 def train_one_epoch(model: nn.Module, dataloader: DataLoader, loss_function: nn.Module, optimizer: torch.optim,
@@ -57,7 +68,6 @@ def train_one_epoch(model: nn.Module, dataloader: DataLoader, loss_function: nn.
     """
     model.train()
     metric_log = MetricLog(epoch=epoch)
-    progress_log = ProgressLog(epoch=epoch, total_step=len(dataloader))
 
     with tqdm(total=len(dataloader)) as t:
         for i, batch in enumerate(dataloader):
@@ -71,17 +81,55 @@ def train_one_epoch(model: nn.Module, dataloader: DataLoader, loss_function: nn.
             loss.backward()
             optimizer.step()
 
-            metric_log.update("loss", loss.item(), count=len(labels))
-            metric_log.update("acc", sum(torch.argmax(labels, dim=1) == torch.argmax(outputs, dim=1)).item() / len(labels),
+            metric_log.update("train_loss", loss.item(), count=len(labels))
+            metric_log.update("train_acc",
+                              sum(torch.argmax(labels, dim=1) == torch.argmax(outputs, dim=1)).item() / len(labels),
                               len(labels))
             metric_log.mean()
 
-            progress_log.update(current_step=i)
             t.set_description("Train")
-            t.set_postfix(loss="%.3f" % metric_log.mean_metrics["loss"],
-                          acc="%.2f%%" % (metric_log.mean_metrics["acc"]*100))
+            t.set_postfix(loss="%.3f" % metric_log.mean_metrics["train_loss"],
+                          acc="%.2f%%" % (metric_log.mean_metrics["train_acc"] * 100))
             t.update(1)
             # print("\r%s" % metric_log.mean_metrics)
             # print(i, "/", len(dataloader))
 
-    # print("!")
+    return metric_log
+
+
+@torch.no_grad()
+def evaluate(model: nn.Module, dataloader: DataLoader, loss_function: nn.Module,
+             config: dict):
+    """
+
+    Args:
+        model:
+        dataloader:
+        loss_function:
+        config:
+
+    Returns:
+
+    """
+    model.eval()
+    metric_log = MetricLog()
+
+    with tqdm(total=len(dataloader)) as t:
+        for i, batch in enumerate(dataloader):
+            images, labels = batch
+            outputs = model(images)
+            labels = torch.from_numpy(labels_to_one_hot(labels, config["DATA"]["CLASS_NUM"]))
+            loss = loss_function(outputs, labels)
+
+            metric_log.update("test_loss", loss.item(), count=len(labels))
+            metric_log.update("test_acc",
+                              sum(torch.argmax(labels, dim=1) == torch.argmax(outputs, dim=1)).item() / len(labels),
+                              len(labels))
+            metric_log.mean()
+
+            t.set_description("Test")
+            t.set_postfix(loss="%.3f" % metric_log.mean_metrics["test_loss"],
+                          acc="%.2f%%" % (metric_log.mean_metrics["test_acc"] * 100))
+            t.update(1)
+
+    return metric_log
