ef80569206227f2b535e22be75f18e1aa889bcfc
HELLORPG <hellorpg2017@gmail.com>
2022-07-11 20:45:08 +0800

    Add a shell script for single gpu mode.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 # 用于测试整个框架的运行
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-python -m torch.distributed.run --nproc_per_node=7 main.py --mode train --outputs-dir ./outputs/dis_tmp --use-distributed True
+#python -m torch.distributed.run --nproc_per_node=7 main.py --mode train --outputs-dir ./outputs/dis_tmp --use-distributed True
+
+# 单卡测试
+python main.py --mode train --outputs-dir ./outputs/single_gpu --use-distributed False
