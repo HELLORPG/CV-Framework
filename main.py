9564af5e5e61b757eed0341dc7c2ee2b9a22b5fb
HELLORPG <hellorpg2017@gmail.com>
2022-10-11 21:56:44 +0800

    Add some TODOs.

--- main.py
+++ main.py
@@ -58,6 +58,13 @@ def main(config: dict):
     Args:
         config: Model configs.
     """
+    # 有部分在科研工作中引入的经验用于修改代码结构：
+    # TODO 1：MetricLog 的 Smooth 长度不应该是无限制的，应该引入一个 max_length 用于限制，将列表改变为一个队列实现。
+    # TODO 2：MetricLog 不应该在每一个 Epoch 内进行初始化，容易带来指标抖动问题，应该作为参数传递进入 Train One Epoch 等函数。
+    # TODO 3：Logger 可以考虑不从 main 函数中传入 train，可以考虑在 train/eval 等函数内部初始化。
+    # TODO 4：应该在每一个 Epoch 内部增加一个统计当前的 Epoch 的指标结果。
+    # TODO 5：还是应该不要引入 'Epoch+1' 这类代码，虽然符合直觉，但是可能不符合代码的逻辑。
+    # TODO 6：ProcessLog 可以删除，采用考虑手动计算 rest time 的方式来体现时间进度。
     # 在环境变量级别设定可用 GPUs
     os.environ['CUDA_VISIBLE_DEVICES'] = config["AVAILABLE_GPUS"]
     logger = Logger(logdir=config["OUTPUTS"]["OUTPUTS_DIR"])
